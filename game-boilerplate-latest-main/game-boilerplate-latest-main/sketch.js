8156d2412530861b6db6d8bbcbce4302aabb57a2
NarsimhaReddy1 <146730584+NarsimhaReddy1@users.noreply.github.com>
2023-10-02 18:01:59 +0530

    Add files via upload

---
+++ game-boilerplate-latest-main/game-boilerplate-latest-main/sketch.js
@@ -0,0 +1,25 @@
+
+var splashimg,playButton,nextButton
+var gameState="wait"
+
+function preload(){
+splashimg=loadImage("")
+
+}
+
+function setup(){
+createCanvas(windowWidth,windowHeight)
+
+}
+
+
+function draw(){
+
+if (gameState=="wait"){
+    background(splashimg)
+}
+
+
+playButton
+
+}
